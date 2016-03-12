0788e0f460a159b5adcf3cce5ac9237771284f1e
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-11 22:03:49 -0800

    Update canvas to contain all circles and move in randomized manner.

--- client/watchout.js
+++ client/watchout.js
@@ -1,11 +1,20 @@
 // start slingin' some d3 here.
 
+var width = 600;
+var height = 480;
+
+var w = d3.select('svg')
+  .style({'width': width});
+
+var h = d3.select('svg')
+  .style({'width': height});
 
 //Title
 d3.select('.header')
   .append('p')
   .append('text')
   .text('ARUN & JEREMY\'S EPIC PINBALL GAME!');
+
 
 //Container
 var container = d3.select('.container')
@@ -20,8 +29,8 @@ var createEnemies = function(n) {
   return _.range(0, n).map(function(i) {
     return {
       id: i,
-      x: parseFloat(Math.random() * 100).toFixed(3),
-      y: parseFloat(Math.random() * 100).toFixed(3)
+      x: parseFloat(Math.random() * 2000).toFixed(3), //fix width
+      y: parseFloat(Math.random() * 2000).toFixed(3) //fix height
     };
   });
 };
@@ -46,6 +55,29 @@ d3.select('svg').selectAll('circle')
 
 //axes
 var axes = {
-  x: d3.scale.linear().domain([0, 100]).range([0, 500]),
-  y: d3.scale.linear().domain([0, 100]).range([0, 500])
+  x: d3.scale.linear().domain([0, width]).range([0, 500]),
+  y: d3.scale.linear().domain([0, height]).range([0, 500])
 };
+
+//Update Function
+var update = function(data) {
+  // DATA JOIN
+  var circle = d3.select('svg').selectAll('circle') 
+    .data(data);
+
+  // UPDATE
+  circle.transition()
+      .duration(750)
+      .attr('cx', function(d) {
+        return axes.x(d.x);
+      })
+      .attr('cy', function(d) {
+        return axes.y(d.y);
+      });
+};
+
+//update(dataset);
+
+setInterval(function() {
+  update(d3.shuffle(dataset));
+}, 1000);
